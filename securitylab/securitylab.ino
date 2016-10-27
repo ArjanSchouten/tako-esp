a5485faafe8616d5549f63b0e0d445b3213a5879
Nick de Ronde <nick3085@gmail.com>
2016-10-27 10:09:55 +0200

    button logic and leds added

--- securitylab/securitylab.ino
+++ securitylab/securitylab.ino
@@ -1,3 +1,7 @@
+#define FASTLED_ESP8266_RAW_PIN_ORDER
+#define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#define FASTLED_ESP8266_D1_PIN_ORDER
+
 #include "storage.cpp"
 #include "http.cpp"
 #include <ESP8266WiFi.h>
@@ -6,21 +10,35 @@
 #include <ArduinoOTA.h>
 /* include button library */
 #include <AnalogMultiButton.h>
+/* include led library */
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
 
 // Debug on=1 debug off=0
 #define DEBUG=1
+
+//Define property's for led strip
+#define DATA_PIN    5
+//#define CLK_PIN   4
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    6
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND  120
 
 const String version = "1.0.0";
 
 // Buttons Pin
 const int BUTTONS_PIN = A0;
 // buttons arary
-const int BUTTONS_TOTAL = 3;
-const int BUTTONS_VALUES[BUTTONS_TOTAL] = {5, 313, 475};
+const int BUTTONS_TOTAL = 2;
+const int BUTTONS_VALUES[BUTTONS_TOTAL] = {313, 475};
 // button names
 const int BUTTON_RECIEVE = 0;
 const int BUTTON_SEND = 1;
-const int BUTTON_RESET = 2;
 
 // make an AnalogMultiButton object, pass in the pin, total and values array
 AnalogMultiButton buttons(BUTTONS_PIN, BUTTONS_TOTAL, BUTTONS_VALUES);
@@ -54,8 +72,8 @@ void setup() {
 #ifdef DEBUG
   deviceId.deviceId = "test";
 
-  wifiSettings.ssid = "ESP_AP";
-  wifiSettings.password = "testtest";
+  wifiSettings.ssid = "Stads-Lab";
+  wifiSettings.password = "initialkey4iot";
 #endif
 
   storage.read<Storage::DeviceId>(Storage::DEVICEID, &deviceId);
@@ -79,36 +97,75 @@ void setup() {
       Serial.print(".");
     }
   }
+  
+  // LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+  
 }
 
 void loop() {
   // update the AnalogMultiButton object every loop
   buttons.update();
 
-  if(buttons.isPressed(BUTTON_RECIEVE))
+//  Debugging buttons 
+//  Serial.println(analogRead(A0));
+
+  if(buttons.onPress(BUTTON_RECIEVE))
   {
      Serial.println("Receive message");
-  } 
+    
+    for(int i=0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Orange;
+      FastLED.show();
+      delay(1000);
+    }
+    delay(2000);
+    for(int i=0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Red;
+      FastLED.show();
+    } 
+    delay(3000);
+    
+    for(int k=0; k < NUM_LEDS; k++) {
+      leds[k] = CRGB::Black;
+      FastLED.show();
+    }
+  }
 
-  // check if BUTTON_GREEN has just been pressed this update
+  // check if BUTTON_SEND has just been pressed
   if(buttons.isPressed(BUTTON_SEND))
   {
-    Serial.println("Message is Send");
-  }
-
-  // check if BUTTON_GREEN has just been pressed this update
-  if(buttons.isPressed(BUTTON_RESET))
-  {
-    Serial.println("Reset Tako");
+    Serial.println("Recording Message");
+    for(int i=0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Blue;
+      FastLED.show();
+    }
   }
   
+  if (buttons.onRelease(BUTTON_SEND)) 
+  { 
+    Serial .println("Message is Recorded, Send message"); 
+    for(int i=0; i < NUM_LEDS; i++) {
+      leds[i] = CRGB::Green;
+      FastLED.show();
+    }
+    
+    delay(2000);
+    
+    for(int k=0; k < NUM_LEDS; k++) {
+      leds[k] = CRGB::Black;
+      FastLED.show();
+    }
+  }
+
   //Wifi accespoint code
-  server.handleClient();
+//   server.handleClient();
 
-  Http::PingResult result = http.pingServer(deviceId.deviceId, version);
+//   Http::PingResult result = http.pingServer(deviceId.deviceId, version);
 
-  
 
-  delay(1000);
+
+  delay(100);
 }
-
